a6b99f89bf62160a6303155f3ee054e104fcb835
geplopez08 <geplopez@outlook.com>
2024-11-14 22:05:38 -0600

    Update Tarea5_Giovanni.R

--- Tarea5_Giovanni.R
+++ Tarea5_Giovanni.R
@@ -14,3 +14,21 @@ arbol <- rpart(DEPARTAMENTO ~
 
 rpart.plot(arbol, type=2, extra=0, under = TRUE, fallen.leaves = TRUE, box.palette = "BuGn", 
            main ="Predicción de departamento", cex = 1)
+
+arbol2 <- rpart(AREA ~
+                  PCH15+
+                  PCH1+
+                  PCH8,
+                data = hogar, method = "class")
+
+rpart.plot(arbol2, type=2, extra=0, under = TRUE, fallen.leaves = TRUE, box.palette = "BuGn", 
+           main ="Predicción de área", cex = 1)
+
+arbol3 <- rpart(PCH5 ~
+                  PCH9_F+
+                  PCH9_K+
+                  PCH10,
+                data = hogar, method = "class")
+
+rpart.plot(arbol3, type=2, extra=0, under = TRUE, fallen.leaves = TRUE, box.palette = "BuGn", 
+           main ="Predicción de servicio sanitario", cex = 1)
